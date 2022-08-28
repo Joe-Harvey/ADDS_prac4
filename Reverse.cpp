d68e02bf7d27e96f41d04299bd514fbaff758aec
Joe-Harvey <joe44harvey@gmail.com>
2022-08-28 23:57:39 +0930

    updates

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,5 +1,4 @@
 #include "Reverse.h"
-#include <string>
 using namespace std;
 
 int Reverse :: reverseDigit(int value){
@@ -8,10 +7,10 @@ int Reverse :: reverseDigit(int value){
 
     //get digits in value
 
-    int n = size(s_value);
+    int n = s_value.size();
 
     //base case
-    if (size <= 1){
+    if (n <= 1){
         return value;
     }
 
@@ -19,13 +18,44 @@ int Reverse :: reverseDigit(int value){
     if (n > 1){
 
         //store and erase first digit
-        string s = s_value.at(0);
-        s_value.erase(0);
+        char c = s_value.at(0);
+        s_value.erase(0, 1);
 
         //call function and add first digit to the end
-        int val = stoi(s_value);
-        string n_string = to_string(reverseDigit(s_value));
-        n_string += s;
+        int n_value = stoi(s_value);
+        string n_string = to_string(reverseDigit(n_value));
+        n_string += c;
+
         return stoi(n_string);
     }
+
+    return 0;
+}
+
+string Reverse :: reverseString(string letters){
+    
+    //get size of string
+
+    int n = letters.size();
+
+    //base case
+    if (n <= 1){
+        return letters;
+    }
+
+    //recursive case
+    if (n > 1){
+
+        //store and erase first character
+        char c = letters.at(0);
+        letters.erase(0, 1);
+
+        //call function and add first character to the end
+        string n_letters = reverseString(n_letters);
+        n_letters += c;
+
+        return n_letters;
+    }
+
+    return "error";
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <string>
+#include "Reverse.h"
 using namespace std;
 
 int main(){
 
     Reverse r;
     int val = 123456;
-    cout << r.reverseDigit(val); << endl;
+    cout << r.reverseDigit(val) << endl;
+
+    string str = "espanol";
+    cout << r.reverseString(str) << endl;
 
     return 0;
 }
